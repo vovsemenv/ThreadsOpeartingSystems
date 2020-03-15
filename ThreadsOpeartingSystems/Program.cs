da3eb6f7746905bb326fad7a2caf330ac325ef75
vovsemenv <20096510+vovsemenv@users.noreply.github.com>
2020-03-15 19:06:41 +0300

    Add project files.

---
+++ ThreadsOpeartingSystems/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ThreadsOpeartingSystems
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+            MainMenu();
+            do {
+                Console.ReadKey();
+                Console.Clear();
+                Thread.Sleep(Demon());
+                Console.WriteLine("Press enter to run again of any other key to close program");
+                key = Console.ReadKey();
+            } while (key.Key == ConsoleKey.Enter);
+
+
+
+        }
+        public static int Demon()
+        {
+            var threads = new ThreadList();
+            threads.start();
+            return threads.max+100;
+        }
+
+        public static void MainMenu()
+        {
+            Console.WriteLine("Threads demonstration created by Vladimir Semenov");
+            Console.WriteLine("For start demonstraion with random times press any key");
+        }
+
+            public static void Call()
+        {
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var thread = new Thread(new ThreadStart(() =>
+                {
+                    var rnd = new Random().Next(1000, 5000);
+                    Console.WriteLine($"{rnd} started");
+                    Thread.Sleep(rnd);
+                    Console.WriteLine($"{rnd} done");
+                }
+                ));
+                threads.Add(thread);
+            }
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ ThreadsOpeartingSystems/ThreadList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadsOpeartingSystems
+{
+    class ThreadList
+    {
+        int en = 0;
+        List<SpecifiedThread> batch= new List<SpecifiedThread>();
+        public int max = 0;
+        public ThreadList()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var th = new SpecifiedThread(i);
+                batch.Add(th);
+                if (th.duration > max) max = th.duration;
+            
+            }
+
+        }
+        public void start()
+        {
+            Console.WriteLine("All threads started");
+            foreach (var thread in batch)
+            {
+                
+                Console.WriteLine($"Thread {thread.cnt} STARTED for {thread.duration}ms");
+                thread.Thread.Start();
+                
+            }
+
+        }
+
+    }
+    class SpecifiedThread
+    {
+        public Thread Thread;
+        public int duration;
+        public int cnt;
+        public SpecifiedThread(int i) { 
+            duration = new Random().Next(1, 5000);
+            cnt = i;
+            Thread = new Thread(() => DoMethod(duration, i));
+        }
+        void DoMethod(int duration, int cnt) {
+                
+                Thread.Sleep(duration);                
+                Console.WriteLine($"Thread {cnt} DONE in {duration}ms"); 
+                while (Console.KeyAvailable) Console.ReadKey(true);
+        }
+    }
+
+
+}
